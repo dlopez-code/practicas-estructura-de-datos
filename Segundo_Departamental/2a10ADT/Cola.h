65aade7766c1dd1230807bacfcec421ad6729224
Diego López <diegole707.dl@gmail.com>
2026-05-01 20:59:05 -0600

    se movio carpeta de ADT fuera del repo

--- Segundo_Departamental/2a10ADT/Cola.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLA_H
-#define COLA_H
-
-#include "ADT.h"
-
-class Cola : public ADT {
-};
-
-#endif
--- Segundo_Departamental/2a10ADT/Lista.h
+++
@@ -1,9 +0,0 @@
-#ifndef LISTA_H
-#define LISTA_H
-
-#include "ADT.h"
-
-class Lista : public ADT {
-};
-
-#endif
